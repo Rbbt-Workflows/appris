850129fc62a0bf5308d580df656a12593d448eb2
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2017-09-27 09:35:13 +0200

    New version of data file

--- lib/rbbt/sources/appris.rb
+++ lib/rbbt/sources/appris.rb
@@ -12,7 +12,7 @@ module Appris
   end
 
   Appris.claim Appris.principal_isoforms, :proc do
-    url = "http://apprisws.bioinfo.cnio.es/pub/current_release/datafiles/homo_sapiens/g19v22/appris_data.principal.txt"
+    url = "http://apprisws.bioinfo.cnio.es/pub/current_release/datafiles/homo_sapiens/g19v24/appris_data.principal.txt"
     tsv = TSV.open(url, :key_field => 1, :fields => [2], :type => :flat, :merge => true, :grep => "PRINCIPAL")
     tsv.key_field = "Ensembl Gene ID"
     tsv.fields = ["Ensembl Transcript ID"]
